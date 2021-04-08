7d2fd961e6d8ad8f347e66362a0f9a98c517d528
retiredtab-gerrit <clrh1tablet@gmail.com>
2021-04-07 21:18:12 -0600

    gtelwifiue: decommonize mixer_paths.xml make it device specific

--- device.mk
+++ device.mk
@@ -6,6 +6,15 @@ $(call inherit-product, device/samsung/gte-common/device-common.mk)
 
 LOCAL_PATH := device/samsung/gtelwifiue
 
+# Mixer_paths.xml needed otherwise android.hardware.audio.service will crash and cause stuck at bootanimation
+#
+# E audio_route: Failed to open /vendor/etc/mixer_paths.xml: No such file or directory
+# E msm8916_platform: platform_init: Failed to init audio route controls, aborting.
+# E audio_hw_primary: adev_open: Failed to init platform data, aborting.
+# libc    : Fatal signal 11 (SIGSEGV), code 1 (SEGV_MAPERR), fault addr 0xa75cd0a4 in tid 689 (audio.service), pid 689 (audio.service)
+PRODUCT_COPY_FILES += \
+    device/samsung/msm8916-common/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
 
