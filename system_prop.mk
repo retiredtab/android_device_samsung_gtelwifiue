0965e9c3352a18071c280a2f8cf22beebd5c2c46
zqm64 <zqm64@outlook.com>
2021-04-07 21:24:33 -0600

    gtelwifiue: add opengles property so more apps are available on play store

--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,7 @@
+# Google Play Store patch to see all apps
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.opengles.version=196608
+
 # Set build fingerprint / ID / Product Name for CTS spoofing
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRIVATE_BUILD_DESC="gtelwifiue-user 7.1.1 NMF26X T560NUUEU1CQK2 release-keys"
